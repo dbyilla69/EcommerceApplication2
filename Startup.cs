6b253d8ed8f1a1ef6dc0a94c07e98e9b88179790
dbyilla69 <doran@eduphoria.net>
2017-09-17 15:49:21 -0500

    Fix errors to get api working

--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using EcommerceApplication2.DataContext;
+using EcommerceApplication2.DataContext.UnitOfWork;
+using EcommerceApplication2.Entities;
+using EcommerceApplication2.Service.Interface;
+using EcommerceApplication2.Service.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,9 +30,21 @@ namespace EcommerceApplication2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<PictureSettings>(Configuration.GetSection("PictureSettings"));
             services.AddMvc();
             services.AddAutoMapper();
+
+             services.AddDbContext<EcommerceContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            //Mapping Repository Services
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IPictureRepository, PictureRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IPictureService, PictureService>();
+            services.AddTransient<IPictureStorage, PictureStorage>();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
